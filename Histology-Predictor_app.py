75b7aa7742bddd401153ff244a9d9b02a1315210
sagarkumar2804 <45657401+sagarkumar2804@users.noreply.github.com>
2021-06-26 13:39:57 +0530

    Update Histology-Predictor_app.py

--- Histology-Predictor_app.py
+++ Histology-Predictor_app.py
@@ -49,7 +49,7 @@ else:
     img_reshape = image[np.newaxis,...]
 
     predictions = model.predict(img_reshape)
-    print(predictions)
+    st.write(predictions)
     score = tf.nn.softmax(predictions)
     # prediction = import_and_predict(imageI, model)
     # pred = prediction[0][0]
