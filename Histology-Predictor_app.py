a6528d931f94b00ec0e3c052b3891a5f190fda8d
sagarkumar2804 <45657401+sagarkumar2804@users.noreply.github.com>
2021-06-26 13:51:13 +0530

    Update Histology-Predictor_app.py

--- Histology-Predictor_app.py
+++ Histology-Predictor_app.py
@@ -52,7 +52,7 @@ else:
     predictions = model.predict(img_reshape)
     for i in range (0,7):
 	if predictions(i)<0:
-		predictions[i]=-(predictions(i))
+		predictions[i]=-(predictions[i])
     st.write(predictions)
     score = tf.nn.softmax(predictions[0])
     st.write(score)
